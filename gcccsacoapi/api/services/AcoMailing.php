4fe49b2632a8a638e9a86ad9cf82b598aa92aba4
dashstar <79578145+kkazei@users.noreply.github.com>
2024-03-30 16:08:18 +0800

    Mailing Update
    Multiple Mailing

--- gcccsacoapi/api/services/AcoMailing.php
+++ gcccsacoapi/api/services/AcoMailing.php
@@ -9,31 +9,35 @@ require_once(__DIR__ . '/../../PHPMailer/src/SMTP.php');
 require_once(__DIR__ . '/../../vendor/autoload.php');
 
 class Mail{
-
     function sendEmail($data){
-
         try {
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'appgradesolutionsgcccsaco@gmail.com';
+            $mail->Password = 'kajwsvjaxeugbwzk';
+            $mail->Port = 465;
+            $mail->SMTPSecure = 'ssl';
+            $mail->isHTML(true);
+            $mail->setFrom('appgradesolutionsgcccsaco@gmail.com');
             
-        $mail = new PHPMailer(true);
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'appgradesolutionsgcccsaco@gmail.com';
-        $mail->Password = 'kajwsvjaxeugbwzk';
-        $mail->Port = 465;
-        $mail->SMTPSecure = 'ssl';
-        $mail->isHTML(true);
-        $mail->setFrom('appgradesolutionsgcccsaco@gmail.com');
-        $mail->addAddress($data->email);
-        $mail->Subject = $data->subject;
-        $mail->Body = $data->message;
-        $mail->send();
-        
-        http_response_code(200);
+            // Split multiple email addresses into an array
+            $emails = explode(',', $data->email);
+            
+            foreach ($emails as $email) {
+                $mail->addAddress(trim($email)); // Trim to remove any extra whitespace
+            }
+
+            $mail->Subject = $data->subject;
+            $mail->Body = $data->message;
+            $mail->send();
+            
+            http_response_code(200);
             //echo json_encode(array("message" => "Email sent successfully."));
-        }catch(Exception $e){
+        } catch(Exception $e){
             http_response_code(500);
             //echo json_encode(array("error" => "Error sending email: " . $e->getMessage()));
         }
-}
+    }
 }
