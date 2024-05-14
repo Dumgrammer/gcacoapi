901c45d40597fac1c98f14e15b574f1d7404f18b
Arankarl <bastianlacap55@gmail.com>
2024-05-14 14:53:38 +0800

    Update AcojwtLogin.php

--- gcccsacoapi/api/services/AcojwtLogin.php
+++ gcccsacoapi/api/services/AcojwtLogin.php
@@ -34,7 +34,6 @@ class Login {
                 'message' => 'Invalid email or password'
             ];
         }
-
         $payload = [
             'iss' => 'localhost',
             'aud' => 'localhost',
