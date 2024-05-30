b97a0b045e3f08775ab94e0f4e130c92d75df84c
Arankarl <bastianlacap55@gmail.com>
2024-05-31 06:41:33 +0800

    Router

--- gcccsacoapi/api/Router.php
+++ gcccsacoapi/api/Router.php
@@ -68,95 +68,33 @@
     require_once('./services/AcoFamily.php');
     require_once('./services/AcoStatistics.php');
 
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');
-}
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, PUT,DELETE,OPTIONS");         
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    exit(0);
-}
-
-// Your main code logic here
-
-
-$conn = new DatabaseAccess();
-$pdo = $conn->connect();
-
-$register = new RegisterUser($pdo);
-$login = new Login($pdo);
-$mail = new Mail($pdo);
-$forms = new FormHandler($pdo);
-$emails = new EmailHandler($pdo);
-$history = new HistoryHandler($pdo);
-$family = new FamilyHandler($pdo);
-$statistics = new StatisticsHandler($pdo);
-
-if (isset($_REQUEST['request'])) {
-    $request = explode('/', $_REQUEST['request']);
-} else {
-    echo "Not Found";
-    http_response_code(404);
-    exit;
-}
-
-switch ($_SERVER['REQUEST_METHOD']) {
-    case 'POST':
-        $data = json_decode(file_get_contents("php://input"));
-        switch ($request[0]) {
-            case 'login':
-                echo json_encode($login->loginUser($data->email, $data->password));
-                break;
-            case 'logout':
-                echo json_encode($login->logoutUser($data));
-                break;
-            case 'register':
-                echo json_encode($register->registerUser($data));
-                break;
-            case 'addrecord':
-                echo json_encode($forms->submitFormData($data));
-                break;
-            case 'editrecord':
-                if (isset($request[1])) {
-                    echo json_encode($forms->updateFormData($request[1], $data));
-                } else {
-                    echo json_encode($forms->sendErrorResponse("Invalid Response", 400));
-                }
-                break;
-            case 'deleterecord':
-                echo json_encode($forms->deleteFormData($data->recordIds));
-                break;
-            case 'mail':
-                echo json_encode($mail->sendEmail($data));
-                break;
-            case 'schedule':
-                echo json_encode($mail->scheduledSend($data));
-                break;
-            case 'mailhistory':
-                echo json_encode($emails->mailHistory($data));
-                break;
-            case  'addrate':
-                echo json_encode($statistics->insertRate($data));
-                break;
-            case 'importData':
-                echo json_encode($forms->importData($data));
-                break;
-            default:
-                echo "This is forbidden";
-                http_response_code(403);
-                break;
-        }
-        break;
-        case 'GET':
+    
+    $con = new DatabaseAccess();
+    $pdo = $con->connect();
+    
+    $register = new RegisterUser($pdo);
+    $login = new Login($pdo);
+    $forms = new FormHandler($pdo);
+    $mail = new Mail($pdo);
+    $emails = new EmailHandler($pdo);
+    $history = new HistoryHandler($pdo);
+    $family = new FamilyHandler($pdo);
+    $statistics = new StatisticsHandler($pdo);
+    
+    // Check if 'request' parameter is set in the request
+    if (isset($_REQUEST['request'])) {
+        // Split the request into an array based on '/'
+        $request = explode('/', $_REQUEST['request']);
+    } else {
+        // If 'request' parameter is not set, return a 404 response
+        echo json_encode(["error" => "Not Found"]);
+        http_response_code(404);
+        exit();
+    }
+    
+    // Handle requests based on HTTP method
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'POST':
             $data = json_decode(file_get_contents("php://input"));
             switch ($request[0]) {
                 case 'login':
